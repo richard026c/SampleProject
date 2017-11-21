41ae704ac91b5e9906cab40d0ff87fb35c4ad949
Richard.Cañeta <richard.caneta@pointwest.com.ph>
2017-11-21 19:42:27 +0800

    Added Test file

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Employee {
+
+	private int employeeID;
+	
+	public Employee() {
+		this..employeeID = 1;
+	}
+	
+	public void setEmployeeId(int employeeID){
+		this.employeeID = employeeID;
+	}
+	
+	public int getEmployeeId(){
+		return employeeID;
+	}
+}
